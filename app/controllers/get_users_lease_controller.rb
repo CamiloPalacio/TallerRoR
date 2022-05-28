a02e97709b6f0dd9b43a459b54db45bf4f41dc2f
Camilo Palacio <camilopalacio2k@gmail.com>
2022-05-28 16:17:14 -0500

    task finished

--- app/controllers/get_users_lease_controller.rb
+++ app/controllers/get_users_lease_controller.rb
@@ -1,4 +1,11 @@
 class GetUsersLeaseController < ApplicationController
+
     def index
+        @prestamos = Prestamo.all
+    end
+
+    def getLease
+        @result = Prestamo.where(:usuario_id => params[:id])
+        render json: @result
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :usuarios
   resources :get_users_lease
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'users/lease/:id', to: 'get_users_lease#getLease'
   # Defines the root path route ("/")
   root :to => 'get_users_lease#index'
 end
